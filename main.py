b8cdf7e47bd8a9c44746a558cccf5468d3588904
mbsomeday <137542994@qq.com>
2025-02-13 13:32:10 +0000

    aa

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ model_name = 'vgg16'
 model = vgg16_bn(num_class=2)
 ds_name_list = ['D3']
 batch_size = 32
-epochs = 10
+epochs = 3
 
 training = train_model(model_name, model, ds_name_list, batch_size, epochs)
 
