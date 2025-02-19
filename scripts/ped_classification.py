7a2501f96211510ff6dedf453048fbde64569f52
mbsomeday <137542994@qq.com>
2025-02-19 13:48:53 +0000

    update

--- scripts/ped_classification.py
+++ scripts/ped_classification.py
@@ -19,7 +19,8 @@ DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def get_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', '--ds_name', type=str)
+    parser.add_argument('--train_on', type=str)
+    parser.add_argument('-d', '--ds_name', type=str, help='dataset that the model is tested on')
     parser.add_argument('-b', '--batch_size', type=int, default=4)
     parser.add_argument('--ds_key_name', type=str)
     parser.add_argument('--txt_name', type=str)
@@ -64,20 +65,23 @@ def ped_test(model, ds_name, test_dataset, test_loader):
 if __name__ == '__main__':
 
     args = get_args()
+    train_on = args.train_on
+
     ds_name = args.ds_name
     batch_size = args.batch_size
     ds_key_name = args.ds_key_name
     txt_name = args.txt_name
     # print(ds_key_name)
 
-    test_dataset = my_dataset(ds_name_list=[ds_name], txt_name=txt_name, key_name=ds_key_name)
-    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     model = vgg16_bn(num_class=2).to(DEVICE)
-    print(f"Reload model {PATHS['ped_cls_ckpt'][ds_name]}")
-    ckpt = torch.load(PATHS['ped_cls_ckpt'][ds_name], map_location=DEVICE, weights_only=False)
+    weights_path = PATHS['ped_cls_ckpt'][train_on]
+    print(f"Reload model {weights_path}")
+    ckpt = torch.load(weights_path, map_location=DEVICE, weights_only=False)
     model.load_state_dict(ckpt['model_state_dict'])
 
+    test_dataset = my_dataset(ds_name_list=[ds_name], txt_name=txt_name, key_name=ds_key_name)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
     ped_test(model, ds_name=ds_name, test_dataset=test_dataset, test_loader=test_loader)
 
 
