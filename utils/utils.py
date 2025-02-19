5cdcb86b32cccda235f7e49f21a5507f3f28075c
mbsomeday <137542994@qq.com>
2025-02-19 13:14:01 +0000

    update

--- utils/utils.py
+++ utils/utils.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 def plot_cm(y_true, y_pred, label_names, title='Confusion Matrix'):
     cm = confusion_matrix(y_true, y_pred)
-    print(f'cm: {cm}')
+    print(f'cm:\n {cm}')
     conf_matrix_df = pd.DataFrame(cm, columns=label_names, index=label_names)
     sns.heatmap(conf_matrix_df, annot=True, fmt='d', cmap='Blues')
     plt.title(title)
