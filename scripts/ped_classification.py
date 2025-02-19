2c70086d2d360b47f4faaa3e56e1ca8dd4703639
mbsomeday <137542994@qq.com>
2025-02-19 11:32:21 +0000

    update

--- scripts/ped_classification.py
+++ scripts/ped_classification.py
@@ -5,21 +5,25 @@ from torch.utils.data import DataLoader
 from models.VGG import vgg16_bn
 from data.dataset import my_dataset
 from configs.paths_dict import PATHS
+from utils.utils import plot_cm
 
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('-d', '--ds_name', type=str)
+    parser.add_argument('-b', '--batch_size', type=int, default=4)
 
     args = parser.parse_args()
     return args
 
 
-def ped_test(model, test_dataset, test_loader):
+def ped_test(model, ds_name, test_dataset, test_loader):
     model.eval()
 
     correct_num = 0
+    y_true = []
+    y_pred = []
     with torch.no_grad():
         for idx, data_dict in enumerate(tqdm(test_loader)):
             images = data_dict['image'].to(DEVICE)
@@ -30,8 +34,16 @@ def ped_test(model, test_dataset, test_loader):
 
             correct_num += (ped_pred == ped_labels).sum()
 
+            y_true.extend(ped_labels)
+            y_pred.extend(ped_pred)
+
         test_accuracy = round(correct_num / len(test_dataset), 6)
         print(test_accuracy)
+
+        # 绘制混淆矩阵
+        label_names = ['ped', 'nonPed']
+        title = f'Ped Cls CM on {ds_name}'
+        plot_cm(y_true, y_pred, label_names, title=title)
 
 
 
@@ -39,15 +51,16 @@ if __name__ == '__main__':
 
     args = get_args()
     ds_name = args.ds_name
+    batch_size = args.batch_size
 
     test_dataset = my_dataset(ds_name_list=[ds_name], txt_name='test.txt', key_name='dataset_dict')
-    test_loader = DataLoader(test_dataset, batch_size=4)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size)
 
     model = vgg16_bn(num_class=2)
     ckpt = torch.load(PATHS['ped_cls_ckpt'][ds_name], map_location=DEVICE, weights_only=False)
     model.load_state_dict(ckpt['model_state_dict'])
 
-    ped_test(model, test_dataset=test_dataset, test_loader=test_loader)
+    ped_test(model, ds_name=ds_name, test_dataset=test_dataset, test_loader=test_loader)
 
 
 
---
+++ utils/utils.py
@@ -0,0 +1,32 @@
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_cm(y_true, y_pred, label_names, title='Confusion Matrix'):
+    cm = confusion_matrix(y_true, y_pred)
+    conf_matrix_df = pd.DataFrame(cm, columns=label_names, index=label_names)
+    sns.heatmap(conf_matrix_df, annot=True, fmt='d', cmap='Blues')
+    plt.title(title)
+    plt.ylabel('Label')
+    plt.xlabel('Prediction')
+    plt.savefig(f'{title}.png')
+    plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
