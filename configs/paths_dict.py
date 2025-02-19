864bef1d9594c855823da97d30b2ad7ed39709aa
mbsomeday <137542994@qq.com>
2025-02-19 11:44:27 +0000

    update

--- configs/paths_dict.py
+++ configs/paths_dict.py
@@ -66,17 +66,17 @@ KAGGLE = {
     'ds_cls_ckpt': r'/kaggle/input/stage4-dscls-weights/vgg16bn-dsCls-029-0.9777.pth',
 
     'ped_cls_ckpt': {
-        'D1': r'D:\my_phd\Model_Weights\Stage4\Baseline\vgg16bn-D1-014-0.9740.pth',
-        'D2': r'D:\my_phd\Model_Weights\Stage4\Baseline\vgg16bn-D2-025-0.9124.pth',
-        'D3': r'D:\my_phd\Model_Weights\Stage4\Baseline\vgg16bn-D3-025-0.9303.pth',
-        'D4': r'D:\my_phd\Model_Weights\Stage4\Baseline\vgg16bn-D4-013-0.9502.pth',
-    },
-
-    'autoencoder_ckpt_dict': {
         'D1': r'/kaggle/input/stage3-baselineweights/vgg-D1_ECPDaytime-020-0.97007483.pth',
         'D2': r'/kaggle/input/stage3-baselineweights/vgg-D2_CityPersons-019-0.95496535.pth',
         'D3': r'/kaggle/input/stage3-baselineweights/vgg-D3_ECPNight-028-0.94842410.pth',
         'D4': r'/kaggle/input/stage3-baselineweights/vgg-D4_BDD100K-020-0.91304350.pth'
+    },
+
+    'autoencoder_ckpt_dict': {
+        'D1': r'/kaggle/input/stage5-weights-ldm-d1/D1_epo26_00894.ckpt',
+        'D2': r'/kaggle/input/stage5-weights-ldm-d2/D2_epo59_01239.ckpt',
+        'D3': r'/kaggle/input/stage5-weights-ldm-d3/D3_epo49_01236.ckpt',
+        'D4': r'/kaggle/input/stage5-weights-ldm-d4/D4_epo34_01236.ckpt'
     },
 
 }
