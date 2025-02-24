b1ea3b2996a997f89eacf6fe1ecca2de76cc35bc
mbsomeday <137542994@qq.com>
2025-02-24 15:26:45 +0000

    update

--- utils/utils.py
+++ utils/utils.py
@@ -1,7 +1,13 @@
+import torch
 from sklearn.metrics import confusion_matrix
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+
+from models.VGG import vgg16_bn
+from configs.paths_dict import PATHS
+
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
 
 
 def plot_cm(y_true, y_pred, label_names, title='Confusion Matrix'):
@@ -15,6 +21,31 @@ def plot_cm(y_true, y_pred, label_names, title='Confusion Matrix'):
     # plt.savefig(f'{title}.png')
     print(f'Image save to {title}.png')
     # plt.show()
+
+
+
+def get_ds_model():
+    '''
+        获取 dataset classifier
+    '''
+    model = vgg16_bn(num_class=4)
+    weight_path = PATHS['ds_cls_ckpt']
+    checkpoints = torch.load(weight_path, map_location=DEVICE)
+    model.load_state_dict(checkpoints['model_state_dict'])
+    model.to(DEVICE)
+    return model
+
+
+def get_orgPed_model(ds_name):
+    model = vgg16_bn(num_class=2)
+    weight_path = PATHS['org_dataset'][ds_name]
+    checkpoints = torch.load(weight_path, map_location=DEVICE)
+    model.load_state_dict(checkpoints['model_state_dict'])
+    model.to(DEVICE)
+    return model
+
+
+
 
 
 
