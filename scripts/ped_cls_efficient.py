59ac2907e1f960776c326f41ca71f7120662e24c
mbsomeday <137542994@qq.com>
2025-02-26 10:50:50 +0000

    update

--- scripts/ped_cls_efficient.py
+++ scripts/ped_cls_efficient.py
@@ -1,5 +1,11 @@
-import torchvision.models as visionModels
+# 将上级目录加入 sys.path， 防止命令行运行时找不到包
+import os, sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+root_path = os.path.split(curPath)[0]
+sys.path.append(root_path)
+
 import argparse
+import torchvision.models as visionModels
 
 from data.dataset import get_data
 from training.training import train_model
