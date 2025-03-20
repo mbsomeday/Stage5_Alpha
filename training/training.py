65b9398af1438fa65ad6c8ed3d1be170d9fce5ea
mbsomeday <137542994@qq.com>
2025-03-20 11:50:22 +0000

    update

--- training/training.py
+++ training/training.py
@@ -65,6 +65,8 @@ class train_model():
             out = self.model(images)
             print('测试代码')
             out[0][0].backward()
+            out[0][0].backward()
+
             print('flag')
             print('测试结束')
 
