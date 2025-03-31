03cf8e72f9f47ba1e3336de3bffcb4aca19433e5
mbsomeday <137542994@qq.com>
2025-03-31 21:38:36 +0100

    update

--- training/train_callbacks.py
+++ training/train_callbacks.py
@@ -35,11 +35,11 @@ class EarlyStopping():
         self.best_val_acc = -np.inf
         self.delta = delta
 
-        # print('-' * 20 + 'Early Stopping Info' + '-' * 20)
-        # print('Create early stopping, monitoring [validation accuracy] changes')
-        # print(f'The best {self.top_k} models will be saved to {self.model_save_dir}')
-        # print(f'File saving format: {save_prefix}_epoch_acc.pth')
-        # print(f'Early Stop with patience: {self.patience} ')
+        print('-' * 20 + 'Early Stopping Info' + '-' * 20)
+        print('Create early stopping, monitoring [validation accuracy] changes')
+        print(f'The best {self.top_k} models will be saved to {self.model_save_dir}')
+        print(f'File saving format: {save_prefix}_epoch_acc.pth')
+        print(f'Early Stop with patience: {self.patience} ')
 
         msg = f'The best {self.top_k} models will be saved to {self.model_save_dir}'
         with open(os.path.join(self.model_save_dir, 'EarlyStop.txt'), 'a') as f:
