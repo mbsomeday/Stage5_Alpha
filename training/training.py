f628625eb4c5a5c178eb367bb3a2ca7187a13791
mbsomeday <137542994@qq.com>
2025-04-02 15:28:11 +0100

    update

--- training/training.py
+++ training/training.py
@@ -1047,7 +1047,8 @@ class train_ds_model_alpha():
         val_num_info = [len(self.val_dataset), -1, -1]
 
         self.epoch_logger = Epoch_logger(save_dir=callback_savd_dir, model_name=model_obj.split('.')[-1],
-                                         ds_name_list=ds_name_list, train_num_info=train_num_info, val_num_info=val_num_info
+                                         ds_name_list=ds_name_list, train_num_info=train_num_info, val_num_info=val_num_info,
+                                         task='ds_cls'
                                          )
 
         # -------------------- 如果reload，optmizer，start_epoch等也要重新设置 --------------------
