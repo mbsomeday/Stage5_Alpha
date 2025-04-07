1fae2e38ed594bb8c768b0b6b7bf86845decb90a
mbsomeday <137542994@qq.com>
2025-04-07 14:48:09 +0100

    update

--- models/EfficientNet.py
+++ models/EfficientNet.py
@@ -1,14 +1,11 @@
 from torchvision import models
 
 
-def get_efficient_ds_model():
-    model = models.efficientnet_b0(weights=None, progress=True, num_classes=4)
+def efficientNetB0(num_class):
+    model = models.efficientnet_b0(weights=None, progress=True, num_classes=num_class)
     return model
 
 
-def get_efficient_ped_model():
-    model = models.efficientnet_b0(weights=None, progress=True, num_classes=2)
-    return model
 
 
 
