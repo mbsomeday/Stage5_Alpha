3014cf7ff59d5d0717404ebcc973bac4b31ad50f
mbsomeday <137542994@qq.com>
2025-04-24 21:18:23 +0100

    update

--- utils/utils.py
+++ utils/utils.py
@@ -3,6 +3,7 @@ from sklearn.metrics import confusion_matrix
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from torchvision import utils as vutils
 
 from models.VGG import vgg16_bn
 from configs.paths_dict import PATHS
@@ -48,7 +49,7 @@ def get_orgPed_model(ds_name):
 
 def load_model(model, weights_path):
     print(f'Loading model from {weights_path}')
-    ckpts = torch.load(weights_path, map_location='cuda' if torch.cuda.is_available() else 'cpu')
+    ckpts = torch.load(weights_path, map_location='cuda' if torch.cuda.is_available() else 'cpu', weights_only=True)
     model.load_state_dict(ckpts['model_state_dict'])
     return model
 
@@ -73,8 +74,20 @@ class DotDict(dict):
 
 
 
-
-
+def save_image_tensor(input_tensor: torch.Tensor, filename):
+    """
+    将tensor保存为图片
+    :param input_tensor: 要保存的tensor
+    :param filename: 保存的文件名
+    """
+    assert (len(input_tensor.shape) == 4 and input_tensor.shape[0] == 1)
+    # 复制一份
+    input_tensor = input_tensor.clone().detach()
+    # 到cpu
+    input_tensor = input_tensor.to(torch.device('cpu'))
+    # 反归一化
+    # input_tensor = unnormalize(input_tensor)
+    vutils.save_image(input_tensor, filename)
 
 
 
