40ff98dd66ebce14afa870803348ab056b117db5
mbsomeday <137542994@qq.com>
2025-05-01 16:41:11 +0100

    update

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -47,15 +47,21 @@ save_best_cls = args.save_best_cls
 
 ds_name_list = [ds_name]
 
+# 行人分类baseline,此时无camloss
 ped_training = train_ped_model_alpha(model_obj=model_obj, ds_name_list=ds_name_list, batch_size=batch_size,
                                         reload=reload, epochs=epochs, base_lr=0.01, warmup_epochs=5, lr_patience=5,
-                                        camLoss_coefficient=0.2, ds_model_obj=model_obj
+                                        camLoss_coefficient=None
                                         )
 
-
+ped_training.train_model()
+# 行人分类 cam loss训练
+# ped_training = train_ped_model_alpha(model_obj=model_obj, ds_name_list=ds_name_list, batch_size=batch_size,
+#                                         reload=reload, epochs=epochs, base_lr=0.01, warmup_epochs=5, lr_patience=5,
+#                                         camLoss_coefficient=0.2, ds_model_obj=model_obj
+#                                         )
 # ped_training.train_model()
-ped_training.val_on_epoch_end(-1)
 
+# 数据集分类训练
 # ds_training = train_ds_model_alpha(model_obj=model_obj, batch_size=batch_size, ds_name_list=['D1', 'D2', 'D3', 'D4'], epochs=epochs, base_lr=0.01, warmup_epochs=5, lr_patience=5)
 #
 # ds_training.train_model()
