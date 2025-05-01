2678fd39dad2089bbf8e917a05b122814eba7c94
mbsomeday <137542994@qq.com>
2025-05-01 18:39:41 +0100

    update

--- models/ResNet.py
+++ models/ResNet.py
@@ -2,7 +2,5 @@ from torchvision import models
 
 
 def ResNet34(num_class):
-    model = models.resnet34(weights=None, num_class=num_class)
+    model = models.resnet34(weights=None, num_classes=num_class)
     return model
-
-
