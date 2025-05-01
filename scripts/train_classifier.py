d8d966460b28bd5714d9954f29d53e3eca798cf0
mbsomeday <137542994@qq.com>
2025-05-01 21:00:44 +0100

    update

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -52,13 +52,13 @@ get_gpu_info()
 
 ds_name_list = [ds_name]
 
-# # 行人分类baseline,此时无camloss
-# ped_training = train_ped_model_alpha(model_obj=model_obj, ds_name_list=ds_name_list, batch_size=batch_size,
-#                                         reload=reload, epochs=epochs, base_lr=0.01, warmup_epochs=5, lr_patience=5,
-#                                         camLoss_coefficient=None
-#                                         )
-#
-# ped_training.train_model()
+# 行人分类baseline,此时无camloss
+ped_training = train_ped_model_alpha(model_obj=model_obj, ds_name_list=ds_name_list, batch_size=batch_size,
+                                        reload=reload, epochs=epochs, base_lr=0.01, warmup_epochs=5, lr_patience=5,
+                                        camLoss_coefficient=None
+                                        )
+
+ped_training.train_model()
 
 # 行人分类 cam loss训练
 # ped_training = train_ped_model_alpha(model_obj=model_obj, ds_name_list=ds_name_list, batch_size=batch_size,
@@ -67,10 +67,10 @@ ds_name_list = [ds_name]
 #                                         )
 # ped_training.train_model()
 
-# 数据集分类训练
-ds_training = train_ds_model_alpha(model_obj=model_obj, batch_size=batch_size, ds_name_list=['D1', 'D2', 'D3', 'D4'], epochs=epochs, base_lr=0.01, warmup_epochs=5, lr_patience=5)
-
-ds_training.train_model()
+# # 数据集分类训练
+# ds_training = train_ds_model_alpha(model_obj=model_obj, batch_size=batch_size, ds_name_list=['D1', 'D2', 'D3', 'D4'], epochs=epochs, base_lr=0.01, warmup_epochs=5, lr_patience=5)
+#
+# ds_training.train_model()
 
 
 
