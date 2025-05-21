c5d0fd7c3972ec72b9c6fc3ccf5cad4493525f4b
mbsomeday <137542994@qq.com>
2025-05-21 16:25:46 +0100

    update

--- data/dataset.py
+++ data/dataset.py
@@ -24,7 +24,7 @@ class my_dataset(Dataset):
 
         self.txt_name = txt_name
         self.img_transforms = transforms.Compose([
-            # transforms.GaussianBlur(kernel_size=5),
+            transforms.GaussianBlur(kernel_size=5),
             transforms.ToTensor()
         ])
         self.images, self.ped_labels, self.ds_labels = self.init_ImagesLabels()
