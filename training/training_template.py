1daefa708c113aea0bdc452ee84a6d8232d3f8e3
mbsomeday <137542994@qq.com>
2025-06-10 21:31:40 +0100

    update

--- training/training_template.py
+++ training/training_template.py
@@ -534,7 +534,7 @@ class Ped_Classifier():
             print(tn, fp, fn, tp)
 
             with open(write_to_txt, 'a') as f:
-                f.write('-' * 80)
+                f.write('\n' + '-' * 80 + '\n')
                 f.write(f'{ds_name}, {test_bc}, {tn}, {fp}, {fn}, {tp}\n')
 
 
