7e43114298e21d54a3b9ca20452b9830dffa298b
mbsomeday <137542994@qq.com>
2025-06-11 13:46:53 +0100

    update

---
+++ main_test.py
@@ -0,0 +1,27 @@
+# 将上级目录加入 sys.path， 防止命令行运行时找不到包
+import os, sys
+curPath = os.path.abspath(os.path.dirname(__file__))
+root_path = os.path.split(curPath)[0]
+sys.path.append(root_path)
+
+import argparse
+
+from training.training_template import Ped_Classifier
+from configs.pedCls_args import BaseArgs, TrainArgs, TestArgs
+
+
+
+opts = TestArgs().parse()
+ped_cls = Ped_Classifier(opts=opts)
+ped_cls.test()
+
+
+
+
+
+
+
+
+
+
+
